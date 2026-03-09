53caa72317cd1d0cddade6297de16a1590d166c8
ZaraNaeem1234 <zara.naeem@remelda.com>
2026-03-09 16:52:10 +0500

    vite changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   resolve: {
     alias: [
       { find: 'figma:asset', replacement: path.resolve(__dirname, 'src/figma/assets') }
     ]
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173
   }
 })
